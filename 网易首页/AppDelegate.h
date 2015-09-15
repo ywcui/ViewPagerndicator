a620fa7fc491c3800045fed7b198e9f9790f8ffa
ywcui <991843897@qq.com>
2015-09-15 10:12:19 +0800

    Initial Commit

---
+++ 网易首页/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易首页
+//
+//  Created by City--Online on 15/9/15.
+//  Copyright (c) 2015年 City--Online. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易首页/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易首页
+//
+//  Created by City--Online on 15/9/15.
+//  Copyright (c) 2015年 City--Online. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
